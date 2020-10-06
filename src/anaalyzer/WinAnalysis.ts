7860cd4e4a97728a3da101d6bd98213b73157bf7
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2020-10-06 23:58:14 +0700

    Update team
    change team

--- src/anaalyzer/WinAnalysis.ts
+++ src/anaalyzer/WinAnalysis.ts
@@ -1,6 +1,6 @@
-import { MatchResult } from './../MatchResult';
-import { MatchData } from './../MatchData';
-import { Analyzer } from './../Summary';
+import { MatchResult } from "./../MatchResult";
+import { MatchData } from "./../MatchData";
+import { Analyzer } from "./../Summary";
 
 export class WinAnalysis implements Analyzer {
   constructor(public teamName: string) {}
@@ -9,10 +9,10 @@ export class WinAnalysis implements Analyzer {
     let wins = 0;
 
     for (let match of matches) {
-      if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+      if (match[1] === "Bournemouth" && match[5] === MatchResult.HomeWin) {
         wins++;
       } else if (
-        match[2] === 'Man United' &&
+        match[2] === "Bournemouth" &&
         match[5] === MatchResult.AwayWin
       ) {
         wins++;
